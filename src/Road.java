20108931c4441638792933cd4845a1505781880d
maureenrnx <maureen.renaux@gmail.com>
2024-02-26 15:26:15 +0100

    update Road class

--- src/Road.java
+++ src/Road.java
@@ -1,3 +1,4 @@
+import java.util.Objects;
 
 public class Road {
 
@@ -8,13 +9,44 @@ public class Road {
     public Road(City source, City destination, double distance) {
         this.source = source;
         this.destination = destination;
-        this.distance = Util.distance(source.)
+        this.distance = Util.distance(source.getLatitude(),source.getLongitude(),destination.getLatitude(),destination.getLongitude());
+    }
 
+    public City getSource() {
+        return source;
+    }
 
+    public void setSource(City source) {
+        this.source = source;
+    }
+
+    public City getDestination() {
+        return destination;
+    }
+
+    public void setDestination(City destination) {
+        this.destination = destination;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Road road)) return false;
+        return Double.compare(distance, road.distance) == 0 && Objects.equals(source, road.source) && Objects.equals(destination, road.destination);
+    }
 
-
+    @Override
+    public int hashCode() {
+        return Objects.hash(source, destination, distance);
+    }
 }
 
