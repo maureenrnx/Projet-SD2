b74ddb10543f94705bbdddff89c8585108e2e114
awadieye <awadieye@student.vinci.be>
2024-02-26 14:14:23 +0100

    class Road

--- src/Road.java
+++ src/Road.java
@@ -1,2 +1,15 @@
+import
 public class Road {
+    private final City source;
+    private final City destination;
+    private final double distance;
+
+    public Road(City source, City destination, double distance) {
+        this.source = source;
+        this.destination = destination;
+
+
+    }
+
 }
+
