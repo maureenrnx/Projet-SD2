6177219cc4b90dc7b54e40b1fd53c2b8a6d02934
maureenrnx <maureen.renaux@gmail.com>
2024-02-26 14:39:56 +0100

    constructor and methods

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,14 @@
+import java.io.File;
+
 public class Graph {
 
+
+    public Graph(File cities, File roads) {
+    }
+
+    public void calculerItineraireMinimisantNombreRoutes(String berlin, String madrid) {
+    }
+
+    public void calculerItineraireMinimisantKm(String berlin, String madrid) {
+    }
 }
