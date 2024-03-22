dc85e3f29d2c1dea09fd9a4627167820cfe0526b
GauthierC <gauthier.collard@student.vinci.be>
2024-03-22 15:10:27 +0100

    refactor: optimisation calculerItineraireMinimisantKm

--- src/Graph.java
+++ src/Graph.java
@@ -5,17 +5,14 @@ import java.io.IOException;
 import java.util.*;
 
 public class Graph {
-    private Map<City, City> allRoads;
     private Map<City, Set<Road>> outputRoads;
     private Map<Integer, City> correspondanceIndiceCity;
     private Map<String, City> correspondanceNameCity;
-    public int nbCity = 0;
 
     public Graph(File cities, File roads) {
         outputRoads = new HashMap<City, Set<Road>>();
         correspondanceIndiceCity = new HashMap<Integer, City>();
         correspondanceNameCity = new HashMap<String, City>();
-        allRoads = new HashMap<City, City>();
 
         readCities(cities);
         readRoads(roads);
@@ -25,7 +22,7 @@ public class Graph {
         outputRoads.put(c, new HashSet<>());
         correspondanceIndiceCity.put(c.getId(), c);
         correspondanceNameCity.put(c.getName(), c);
-        nbCity++;
+
     }
 
     public void ajouterArc(Road r) {
@@ -33,64 +30,50 @@ public class Graph {
         s.add(r);
     }
 
-    public boolean sontAdjacents(City c1, City c2) {
-        for (Road road : outputRoads.get(c1)) {
-            if (road.getDestination().equals(c2)) {
-                return true;
-            }
-        }
-
-        for (Road road : outputRoads.get(c2)) {
-            if (road.getDestination().equals(c1)) {
-                return true;
-            }
-        }
-        return false;
-    }
-
     public Set<Road> arcSortants(City c1) {
         return outputRoads.get(c1);
     }
 
-    //BFS
     public void calculerItineraireMinimisantNombreRoutes(String c1, String c2) {
         try {
-            City city1 = correspondanceNameCity.get(c1);
-            City city2 = correspondanceNameCity.get(c2);
+            City city1 = stringToCity(c1);
+            City city2 = stringToCity(c2);
 
-            if (city1 == null || city2 == null) {
-                System.out.println("Villes non trouvées dans le graphe.");
+            if (city1 == null || city2 == null || c1.equals(" ") || c2.equals(" ")) {
+                System.out.println("Paramètres invalides");
                 return;
             }
 
             Queue<City> fileDeSommets = new LinkedList<>();
-            Map<City, Road> predecesseurs = new HashMap<>();
+            Map<City, Road> sommetsVisites = new HashMap<>();
 
             fileDeSommets.offer(city1);
 
+            // Recherche du chemin minimisant le nombre de route a partir des arcSortants
             while (!fileDeSommets.isEmpty()) {
                 City baladeur = fileDeSommets.poll();
 
                 for (Road r : arcSortants(baladeur)) {
                     City voisin = r.getDestination();
 
-                    if (!predecesseurs.containsKey(voisin)) {
+                    if (!sommetsVisites.containsKey(voisin)) {
                         fileDeSommets.offer(voisin);
-                        predecesseurs.put(voisin, r);
+                        sommetsVisites.put(voisin, r);
                     }
                 }
             }
 
-            if (!predecesseurs.containsKey(city2)) {
+            if (!sommetsVisites.containsKey(city2)) {
                 throw new NoSuchElementException();
             }
 
-            LinkedList<Road> chemin = new LinkedList<>();
+            // Construction du chemin à partir des villes visitées
+            List<Road> chemin = new LinkedList<>();
             double distanceTotale = 0;
             City baladeur = city2;
 
             while (baladeur != null) {
-                Road road = predecesseurs.get(baladeur);
+                Road road = sommetsVisites.get(baladeur);
                 if (baladeur == city1) {
                     break;
                 }
@@ -103,64 +86,74 @@ public class Graph {
                 }
             }
 
-            System.out.println("Trajet de " + c1 + " à " + c2 + ": " + chemin.size() + " routes et " + String.format("%.2f", distanceTotale) + " km");
-            for (Road road : chemin) {
-                System.out.println(road.getSource().getName() + " -> " + road.getDestination().getName() + "(" + String.format("%.2f", road.getDistance()) + " km)");
-            }
+            afficherTrajet(chemin,distanceTotale,city1,city2);
+
         } catch (NoSuchElementException e) {
-            System.err.println("Erreur : Aucun chemin trouvé entre " + c1 + " et " + c2);
+            System.err.println("Erreur : Aucun trajet trouvé entre " + c1 + " et " + c2);
         }
     }
 
-    //Dijkstra
-    //Mettre à jour et ajouter une structure triee ou on compare les distances puis comparer sur base du nom (thenCompare)
     public void calculerItineraireMinimisantKm(String c1, String c2) {
         try {
-            City city1 = correspondanceNameCity.get(c1);
-            City city2 = correspondanceNameCity.get(c2);
+            City city1 = stringToCity(c1);
+            City city2 = stringToCity(c2);
 
-            if (city1 == null || city2 == null) {
-                System.out.println("Villes non trouvées dans le graphe.");
+            if (city1 == null || city2 == null || c1.equals(" ") || c2.equals(" ")) {
+                System.out.println("Paramètres invalides");
                 return;
             }
 
-            Map<City, Double> etiquettesProvisoires = new HashMap<>(); // Modifier en TreeSet pour que la structure soit triée par distance puis nom
             Map<City, Double> etiquettesDefinitives = new HashMap<>();
+            TreeSet<City> etiquettesProvisoires = new TreeSet<>(new Comparator<City>() {
+                @Override
+                public int compare(City cityA, City cityB) {
+                    // Comparaison par distance
+                    int comparaisonDistance = Double.compare(etiquettesDefinitives.get(cityA), etiquettesDefinitives.get(cityB));
+                    if (comparaisonDistance != 0) {
+                        return comparaisonDistance;
+                    }
+                    // Si les distances sont égales, comparer par nom
+                    return cityA.getName().compareTo(cityB.getName());
+                }
+            });
             Map<City, Road> villesVisitees = new HashMap<>();
 
+            // Remplissage des etiquettesProvisoires et etiquettes
             for (City city : outputRoads.keySet()) {
                 if (city.equals(city1)) {
                     etiquettesDefinitives.put(city, 0.0);
-                    etiquettesProvisoires.put(city,0.0);
+                    etiquettesProvisoires.add(city);
                 } else {
-                    etiquettesProvisoires.put(city, Double.MAX_VALUE);
                     etiquettesDefinitives.put(city, Double.MAX_VALUE);
+                    etiquettesProvisoires.add(city);
                 }
             }
 
             while (!etiquettesProvisoires.isEmpty()) {
-                City currentCity = getMinDistanceCity(etiquettesProvisoires);
+                City currentCity = etiquettesProvisoires.pollFirst();
                 etiquettesProvisoires.remove(currentCity);
 
                 if (currentCity.equals(city2)) {
                     break;
                 }
 
+                //Recherche de la plus petite distance -> remplissage des éttiquettes
                 for (Road road : outputRoads.get(currentCity)) {
                     City neighborCity = road.getDestination();
                     double distanceToNeighbor = road.getDistance();
                     double distanceThroughCurrent = etiquettesDefinitives.get(currentCity) + distanceToNeighbor;
 
                     if (distanceThroughCurrent < etiquettesDefinitives.get(neighborCity)) {
-                        etiquettesProvisoires.put(neighborCity, distanceThroughCurrent);
+                        etiquettesProvisoires.remove(neighborCity);
                         etiquettesDefinitives.put(neighborCity, distanceThroughCurrent);
+                        etiquettesProvisoires.add(neighborCity);
                         villesVisitees.put(neighborCity, road);
                     }
                 }
             }
 
-            // Construction du chemin à partir des prédécesseurs
-            LinkedList<Road> chemin = new LinkedList<>();
+            // Construction du chemin à partir des villes visitées
+            List<Road> chemin = new LinkedList<>();
             double distanceTotale = 0;
             City currentCity = city2;
 
@@ -175,21 +168,26 @@ public class Graph {
                 }
             }
 
-            System.out.println("Trajet de " + c1 + " à " + c2 + ": " + chemin.size() + " routes et " + String.format("%.2f", distanceTotale) + " km");
-            for (Road road : chemin) {
-                System.out.println(road.getSource().getName() + " -> " + road.getDestination().getName() + "(" + String.format("%.2f", road.getDistance()) + " km)");
-            }
+            afficherTrajet(chemin,distanceTotale,city1,city2);
 
         } catch (NoSuchElementException e) {
-            System.err.println("Erreur : Aucun chemin trouvé entre " + c1 + " et " + c2);
+            System.err.println("Erreur : Aucun trajet trouvé entre " + c1 + " et " + c2);
         }
     }
 
-    // Méthode pour obtenir la ville avec l'étiquette provisoire la plus petite
-    private City getMinDistanceCity(Map<City, Double> etiquettesProvisoires) {
-        return Collections.min(etiquettesProvisoires.entrySet(), Comparator.comparingDouble(Map.Entry::getValue)).getKey();
+    // Méthode pour afficher le résultat demandé
+    public void afficherTrajet(List<Road> chemin, double distanceTotale, City c1, City c2){
+        System.out.println("Trajet de " + c1.getName() + " à " + c2.getName() + ": " + chemin.size() + " routes et " + distanceTotale + " kms");
+        for (Road road : chemin) {
+            System.out.println(road.getSource().getName() + " -> " + road.getDestination().getName() + " (" + String.format("%.2f", road.getDistance()) + " km)");
+        }
     }
 
+    public City stringToCity(String city){
+        return correspondanceNameCity.get(city);
+    }
+
+    //Lecture du fichier cities.txt et remplissage des structures
     public void readCities(File file) {
         try {
             String line;
@@ -214,6 +212,7 @@ public class Graph {
         }
     }
 
+    // Lecture du fichier roads.txt et remplissage des structures
     public void readRoads(File file) {
         try {
             String line;
